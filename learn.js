628e0986ddee1b93813b0ce57bc679eb0b83fce4
ebedinin <bedinin@gmail.com>
2021-11-21 22:20:10 +1000

    17

--- learn.js
+++ learn.js
@@ -1,8 +1,3 @@
-let a = 5;
-let b = 1;
-a++;
-// Only change code below this line
-console.log(a);
-
-let sumAB = a + b;
-console.log(sumAB);
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
