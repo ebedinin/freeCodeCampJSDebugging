c7484fdab6aa7990c6496644616867b71cd580f4
Admin <bedinin@gmail.com>
2021-11-22 17:24:03 +1000

    33

--- learn.js
+++ learn.js
@@ -1,6 +1,4 @@
-let seven = 7;
-let three = "3";
-console.log(typeof(seven));
-console.log(typeof(three));
-console.log(seven + three);
-// Only change code below this line
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
